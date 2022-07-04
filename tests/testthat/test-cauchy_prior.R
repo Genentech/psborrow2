fd47b4cb667be5731211cef532c12ba17b6d1cfe
Secrest, Matt {MDBW~South San Francisco} <35199424+mattsecrest@users.noreply.github.com>
2022-07-04 17:12:20 +0200

    cauchy

---
+++ tests/testthat/test-cauchy_prior.R
@@ -0,0 +1,14 @@
+test_that("Cauchy priors are rendering correctly", {
+
+   # Make normal prior
+   prior <- cauchy_prior(mu = 2, sigma = 3)
+
+   # Expect correct class
+   expect_class(prior, "CauchyPrior")
+   expect_equal(prior@mu, 2L)
+   expect_equal(prior@sigma, 3L)
+
+   # Errors
+   expect_error(cauchy_prior(mu = 2, sigma = -1),
+                regexp = 'invalid class “CauchyPrior” object: sigma must be >0')
+})
