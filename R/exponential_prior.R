a6e5f8b31b0ccda19cedfc72bcd986feaf60dda9
Secrest, Matt {MDBW~South San Francisco} <35199424+mattsecrest@users.noreply.github.com>
2022-07-11 10:17:18 -0700

    exponential prior

---
+++ R/exponential_prior.R
@@ -0,0 +1,31 @@
+#' @include prior.R
+
+# Internal constructor
+.exponential_prior <- setClass(
+   "ExponentialPrior",
+   contains = "Prior",
+   slots = c(beta = "numeric"),
+   prototype = list(
+      n_param = 1L,
+      stan_code = "exponential(beta={{object@beta}})"
+   ),
+   validity = function(object) {
+      if (object@beta <= 0) {
+         return("beta must be >0")
+      }
+      return(TRUE)
+   }
+)
+
+#' Prior exponential distribution
+#'
+#' @param beta inverse scale
+#'
+#' @return object of class "ExponentialPrior"
+#' @export
+#'
+#' @examples
+#' ep <- exponential_prior(1,1)
+exponential_prior <- function(beta) {
+   .exponential_prior(beta = beta)
+}
