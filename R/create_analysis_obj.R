4cfba8127b4d17574e5ad9d0012761e81344a64d
Secrest, Matt {MDBW~South San Francisco} <35199424+mattsecrest@users.noreply.github.com>
2022-07-11 22:17:45 -0700

    wip

--- R/create_analysis_obj.R
+++ R/create_analysis_obj.R
@@ -1,20 +1,42 @@
 create_analysis_obj <- function(
    model_matrix,
-   covariates,
+   covariates = NULL,
    outcome,
    borrowing,
-   treatment_arms
+   treatment_arms,
+   export_stan_model = FALSE,
+   stan_model_path = "./model.stan"
 ) {
 
    # Input checks----
    ## Covariates----
-   if(!is.null(covariates)) {
+   if (!is.null(covariates)) {
       if (!all(covariates@covariates %in% colnames(model_matrix))) {
-         stop("Covariate columns are not all specified in model matrix")
+         stop(paste0(
+            "Covariate columns `",
+            paste0(covariates@covariates[
+               !covariates@covariates %in% colnames(model_matrix)
+            ], collapse = "`, `"),
+            "` are not in the model matrix"))
       }
    }
 
    ## Outcomes----
+   if (!outcome@time_var %in% colnames(model_matrix)) {
+      stop(paste0(
+         "Time variable `",
+         outcome@time_var,
+         "` is not a column in the model matrix"))
+   }
+   if (!outcome@cens_var %in% colnames(model_matrix)) {
+      stop(paste0(
+         "Censor variable `",
+         outcome@cens_var,
+         "` is not a column in the model matrix"))
+   }
+
+
+   message("Inputs look good, time to make the STAN model")
 
 
 }
