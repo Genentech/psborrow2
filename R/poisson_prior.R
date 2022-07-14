28f08f5ba5ec666415e082d31b91e63529721bbe
gravesti <isaac.gravestock@roche.com>
2022-07-14 14:15:16 +0200

    add some docs

--- R/poisson_prior.R
+++ R/poisson_prior.R
@@ -2,30 +2,30 @@
 
 # Internal constructor
 .poisson_prior <- setClass(
-  "PoissonPrior",
-  contains = "Prior",
-  slots = c(lambda = "numeric"),
-  prototype = list(
-    n_param = 1L,
-    stan_code = "poisson({{object@lambda}})"
-  ),
-  validity = function(object) {
-    if (object@lambda <= 0) {
-      return("lambda must be >0")
-    }
-    return(TRUE)
-  }
+   "PoissonPrior",
+   contains = "Prior",
+   slots = c(lambda = "numeric"),
+   prototype = list(
+      n_param = 1L,
+      stan_code = "poisson({{object@lambda}})"
+   ),
+   validity = function(object) {
+      if (object@lambda <= 0) {
+         return("lambda must be >0")
+      }
+      return(TRUE)
+   }
 )
 
 #' Prior poisson distribution
 #'
-#' @param lambda lambda
+#' @param lambda rate (>0)
 #'
 #' @return object of class "PoissonPrior"
 #' @export
-#'
+#' @family priors
 #' @examples
 #' pp <- poisson_prior(100)
 poisson_prior <- function(lambda) {
-  .poisson_prior(lambda = lambda)
+   .poisson_prior(lambda = lambda)
 }
