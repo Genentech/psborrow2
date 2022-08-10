090cf7e4fc0510660a01cd4c8f562b70e5530b58
gravesti <isaac.gravestock@roche.com>
2022-08-10 15:55:17 +0200

    tidy

--- R/add_covariates.R
+++ R/add_covariates.R
@@ -1,3 +1,8 @@
+#' @include covariate_class.R
+NULL
+
+#' Add Covariates for Model Adjustment
+#'
 #' Specify column names for adjustment variables in model matrix and prior
 #' distributions for the model parameters for these covariates (i.e., betas)
 #'
@@ -6,10 +11,8 @@
 #' @param priors either a single prior distribution applying to all covariates
 #' or a named list of prior distributions, one for each covariate
 #'
-#' @return object of class "Covariates"
+#' @return object of class `Covariates`
 #' @export
-#'
-#' @importFrom methods is
 #'
 #' @examples
 #' add_covariates(
@@ -19,20 +22,16 @@
 #'     "b" = normal_prior(0, 2)
 #'   )
 #' )
-#' @include covariate_class.R
 add_covariates <- function(covariates,
                            priors) {
-  # Better messaging for errors that are already considered in class
   if (!is(priors, "listOrPrior")) {
     stop(
       "priors argument must be a single object of class `Prior`",
       " or a named list of objects of class `Prior`"
     )
   }
-
   assert_character(covariates)
 
-  # Create class
   .covariate_class(
     covariates = covariates,
     priors = priors
--- R/covariate_class.R
+++ R/covariate_class.R
@@ -3,7 +3,7 @@
 # Combine class types
 setClassUnion("listOrPrior", c("list", "Prior"))
 
-# Covariate class
+# Covariate class ----
 .covariate_class <- setClass(
   "Covariates",
   slots = c(
@@ -11,36 +11,35 @@ setClassUnion("listOrPrior", c("list", "Prior"))
     priors = "listOrPrior"
   ),
   validity = function(object) {
-    if (is(object@priors, "list") &&
-      NROW(object@priors) != NROW(object@covariates)) {
-      return(paste0(
-        "Either specify 1 prior distribution for all ",
-        "covariates, or specify a named list with 1 prior per ",
-        "covariate"
-      ))
-    }
+    if (is(object@priors, "list")) {
+      if (NROW(object@priors) != NROW(object@covariates)) {
+        return(paste0(
+          "Either specify 1 prior distribution for all ",
+          "covariates, or specify a named list with 1 prior per ",
+          "covariate"
+        ))
+      }
 
-    if (is(object@priors, "list") &&
-      !all(sapply(object@priors, is, "Prior"))) {
-      return(paste0(
-        "If a list is provided to specify priors, all priors ",
-        "must be of class `Prior`"
-      ))
-    }
+      if (!all(vapply(object@priors, is, logical(1L), class2 = "Prior"))) {
+        return(paste0(
+          "If a list is provided to specify priors, all priors ",
+          "must be of class `Prior`"
+        ))
+      }
 
-    if (is(object@priors, "list") &&
-      !all(names(object@priors) %in% object@covariates)) {
-      return(paste0(
-        "If a list is provided to specify priors, one prior per ",
-        "covariate must be provided."
-      ))
+      if (!all(names(object@priors) %in% object@covariates)) {
+        return(paste0(
+          "If a list is provided to specify priors, one prior per ",
+          "covariate must be provided."
+        ))
+      }
     }
 
     return(TRUE)
   }
 )
 
-# Print method
+# show ----
 setMethod(
   f = "show",
   signature = "Covariates",
