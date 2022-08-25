1f1021cd93e52d1430d01447ef041e738d330f7e
gravesti <isaac.gravestock@roche.com>
2022-08-25 09:48:59 +0200

    additional test for trim_data_matrix

--- tests/testthat/test-trim_data_matrix.R
+++ tests/testthat/test-trim_data_matrix.R
@@ -1,4 +1,4 @@
-test_that("data matrix trimming works", {
+test_that("data matrix trimming works with BDB", {
   anls1 <- psborrow2:::.analysis_obj(
     data_matrix = example_matrix,
     covariates = add_covariates(covariates = "cov1", normal_prior(0, 1000)),
@@ -12,6 +12,14 @@ test_that("data matrix trimming works", {
     )
   )
 
+  anls1_trim <- psborrow2:::trim_data_matrix(anls1)
+  expect_matrix(anls1_trim@data_matrix, mode = "numeric", nrows = 50, ncols = 5)
+  expect_set_equal(
+    colnames(anls1_trim@data_matrix), c("time", "cnsr", "cov1", "trt", "ext")
+  )
+})
+
+test_that("data matrix trimming works with Full Borrowing", {
   anls2 <- psborrow2:::.analysis_obj(
     data_matrix = example_matrix,
     outcome = exp_surv_dist("time", "cnsr"),
@@ -23,14 +31,29 @@ test_that("data matrix trimming works", {
     )
   )
 
-  anls1_trim <- psborrow2:::trim_data_matrix(anls1)
   anls2_trim <- psborrow2:::trim_data_matrix(anls2)
-
-  expect_set_equal(
-    colnames(anls1_trim@data_matrix), c("time", "cnsr", "cov1", "trt", "ext")
-  )
-
+  expect_matrix(anls2_trim@data_matrix, mode = "numeric", nrows = 50, ncols = 3)
   expect_set_equal(
     colnames(anls2_trim@data_matrix), c("time", "cnsr", "trt")
+  )
+})
+
+test_that("data matrix trimming works with No Borrowing", {
+  object <- psborrow2:::.analysis_obj(
+    data_matrix = example_matrix,
+    outcome = exp_surv_dist("time", "cnsr"),
+    treatment = treatment_details("trt", trt_prior = normal_prior(0, 1000)),
+    covariates = add_covariates("cov1", normal_prior(0, 1000)),
+    borrowing = borrowing_details(
+      method = "No borrowing",
+      baseline_prior = normal_prior(0, 1000),
+      ext_flag_col = "ext"
+    )
+  )
+
+  result <- psborrow2:::trim_data_matrix(object)
+  expect_matrix(result@data_matrix, mode = "numeric", nrows = 22, ncols = 4)
+  expect_set_equal(
+    colnames(result@data_matrix), c("time", "cnsr", "trt", "cov1")
   )
 })
