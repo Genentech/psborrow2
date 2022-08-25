8612607540f2f1a37a8b2935a53d0198dfa4c77c
Matt Secrest <secrestm@gene.com>
2022-08-25 15:02:50 -0700

    wip mid test

--- tests/testthat/test-mcmc_sample.R
+++ tests/testthat/test-mcmc_sample.R
@@ -11,11 +11,8 @@ test_that("mcmc_sample.default() default method throws error", {
 
 })
 
-test_that("mcmc_sample.Analysis() captures parameters of interest", {
+test_that("mcmc_sample.Analysis() works for full borrowing, exponential dist", {
 
-  # Make comparable estimates----
-
-  ## Full borrowing, exponential distribution ----
   full_exp <- exp(coef(flexsurv::flexsurvreg(
     survival::Surv(time, 1-cnsr) ~ trt,
     data = as.data.frame(example_matrix),
@@ -33,16 +30,19 @@ test_that("mcmc_sample.Analysis() captures parameters of interest", {
 
   full_exp_bayes <- mcmc_sample(full_exp_bayes_ao,
                                 iter_warmup = 1000,
-                                iter_sampling = 100000,
+                                iter_sampling = 10000,
                                 chains = 1)
 
   expect_true(
-    dplyr::near(full_exp_bayes$summary('HR_trt')[,'mean'][[1]],
+    dplyr::near(full_exp_bayes$summary('HR_trt')[,'median'][[1]],
                 full_exp,
-                tol = .05)
+                tol = .02)
   )
 
-  ## No borrowing, exponential distribution ----
+})
+
+test_that("mcmc_sample.Analysis() works for no borrowing, exponential dist", {
+
   int_exp <- exp(coef(flexsurv::flexsurvreg(
     survival::Surv(time, 1-cnsr) ~ trt,
     data = as.data.frame(example_matrix[example_matrix[,"ext"]==0,]),
@@ -60,15 +60,19 @@ test_that("mcmc_sample.Analysis() captures parameters of interest", {
 
   int_exp_bayes <- mcmc_sample(int_exp_bayes_ao,
                                 iter_warmup = 1000,
-                                iter_sampling = 100000,
+                                iter_sampling = 10000,
                                 chains = 1)
 
   expect_true(
-    dplyr::near(full_exp_bayes$summary('HR_trt')[,'mean'][[1]],
-                full_exp,
-                tol = .05)
+    dplyr::near(int_exp_bayes$summary('HR_trt')[,'mean'][[1]],
+                int_exp,
+                tol = .02)
   )
 
+})
+
+test_that("mcmc_sample.Analysis() works for no borrowing, exponential dist,
+          one covariate", {
 
   full_exp_c1 <- exp(coef(flexsurv::flexsurvreg(
     survival::Surv(time, 1-cnsr) ~ trt + cov1,
@@ -76,13 +80,30 @@ test_that("mcmc_sample.Analysis() captures parameters of interest", {
     dist = "exponential"
   ))['trt'])
 
-  custom_weibullPH <- list(name = "weibullPH",
-                           pars = c("shape", "scale"), location = "scale",
-                           transforms = c(log, log),
-                           inv.transforms = c(exp, exp),
-                           inits = function(t){
-                             c(1,1)
-                           })
+  full_exp_bayes_c1_ao <- create_analysis_obj(
+    data_matrix = example_matrix,
+    outcome = exp_surv_dist("time", "cnsr"),
+    borrowing = borrowing_details("Full borrowing",
+                                  normal_prior(0, 100000),
+                                  ext_flag_col = "ext"),
+    treatment = treatment_details("trt", normal_prior(0, 100000))
+  )
+
+  full_exp_bayes_c1 <- mcmc_sample(full_exp_bayes_c1_ao,
+                                   iter_warmup = 1000,
+                                   iter_sampling = 10000,
+                                   chains = 1)
+
+  expect_true(
+    dplyr::near(full_exp_bayes$summary('HR_trt')[,'median'][[1]],
+                full_exp,
+                tol = .02)
+  )
+
+
+
+})
+
   full_weib <- exp(coef(flexsurv::flexsurvreg(
     survival::Surv(time, 1-cnsr) ~ trt,
     data = as.data.frame(example_matrix),
@@ -122,3 +143,12 @@ test_that("mcmc_sample.Analysis() captures parameters of interest", {
   # Benchmark
 
 })
+
+
+custom_weibullPH <- list(name = "weibullPH",
+                         pars = c("shape", "scale"), location = "scale",
+                         transforms = c(log, log),
+                         inv.transforms = c(exp, exp),
+                         inits = function(t){
+                           c(1,1)
+                         })
