4944c423ef3e2ea9dce04dd72aee352b23db3e0f
mattsecrest <secrestm@gene.com>
2022-09-21 08:30:26 -0700

    147 Limit data copying to encourage assignment by reference (#149)
    * WIP, CmdStanModel not importing
    
    * functioning refactor

--- R/prepare_stan_data_inputs.R
+++ R/prepare_stan_data_inputs.R
@@ -3,13 +3,15 @@
 #' Create a list of data in the correct format for Stan
 #'
 #' @param analysis_obj `Analysis`. Object of class [`Analysis`][Analysis-class] created by
-#' `psborrow2:::.analysis_obj()`.
+#' `create_analysis_obj()`.
 #'
 #' @return Named list of data inputs that correspond to items in the
 #' Stan data {} block.
 #'
+#' @include create_analysis_obj.R
+#'
 #' @examples
-#' anls_obj <- psborrow2:::.analysis_obj(
+#' anls_obj <- create_analysis_obj(
 #'   data_matrix = example_matrix,
 #'   outcome = exp_surv_dist("time", "cnsr", normal_prior(0, 100)),
 #'   borrowing = borrowing_details(
@@ -20,37 +22,38 @@
 #'   treatment = treatment_details("trt", normal_prior(0, 100))
 #' )
 #'
-#' anls_obj@model_and_data <- list(
-#'   data_in = psborrow2:::prepare_stan_data_inputs(anls_obj)
-#' )
+#' data_in <- psborrow2:::prepare_stan_data_inputs(anls_obj)
 #'
 prepare_stan_data_inputs <- function(analysis_obj) {
+  ## Trimmed data matrix
+  trimmed_data_matrix <- trim_data_matrix(analysis_obj)
+
   ## Common inputs
   data_in <- list(
-    N = NROW(analysis_obj@data_matrix),
-    trt = analysis_obj@data_matrix[, analysis_obj@treatment@trt_flag_col]
+    N = NROW(trimmed_data_matrix),
+    trt = trimmed_data_matrix[, analysis_obj@treatment@trt_flag_col]
   )
 
   ## Outcome-specific additions
   if (is(analysis_obj@outcome, "TimeToEvent")) {
-    data_in[["time"]] <- analysis_obj@data_matrix[, analysis_obj@outcome@time_var]
-    data_in[["cens"]] <- analysis_obj@data_matrix[, analysis_obj@outcome@cens_var]
+    data_in[["time"]] <- trimmed_data_matrix[, analysis_obj@outcome@time_var]
+    data_in[["cens"]] <- trimmed_data_matrix[, analysis_obj@outcome@cens_var]
   } else if (is(analysis_obj@outcome, "BinaryOutcome")) {
-    data_in[["y"]] <- analysis_obj@data_matrix[, analysis_obj@outcome@binary_var]
+    data_in[["y"]] <- trimmed_data_matrix[, analysis_obj@outcome@binary_var]
   }
 
   ## BDB additions
   if (analysis_obj@borrowing@method == "BDB") {
     data_in[["Z"]] <- cbind(
-      1 - analysis_obj@data_matrix[, analysis_obj@borrowing@ext_flag_col],
-      analysis_obj@data_matrix[, analysis_obj@borrowing@ext_flag_col]
+      1 - trimmed_data_matrix[, analysis_obj@borrowing@ext_flag_col],
+      trimmed_data_matrix[, analysis_obj@borrowing@ext_flag_col]
     ) # Column 1 is the indicator for internal. Column 2 is the external indicator.
   }
 
   ## Covariate additions
   if (!is.null(analysis_obj@covariates)) {
     data_in[["K"]] <- NROW(analysis_obj@covariates@covariates)
-    data_in[["X"]] <- analysis_obj@data_matrix[, analysis_obj@covariates@covariates, drop = FALSE]
+    data_in[["X"]] <- trimmed_data_matrix[, analysis_obj@covariates@covariates, drop = FALSE]
     beta_constraints <- get_covariate_constraints(analysis_obj@covariates)
     data_in[["L_beta"]] <- beta_constraints[, "lower"]
     data_in[["U_beta"]] <- beta_constraints[, "upper"]
