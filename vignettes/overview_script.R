d1e74d08d6d6a18fc74ee1d40ae805b22fe5e524
Matt Secrest <secrestm@gene.com>
2022-10-02 15:22:04 -0700

    gitignore and script add

---
+++ vignettes/overview_script.R
@@ -0,0 +1,114 @@
+############################################################
+#                                                          #
+#               OVERVIEW OF PSBORROW2                      #
+#                  Matthew Secrest                         #
+#                   October 2022                           #
+#                                                          #
+############################################################
+
+# Load dependancies----
+# psborrow2
+library(psborrow2)
+
+# survival analysis
+library(survival)
+library(survminer)
+library(flexsurv)
+
+# plotting
+library(ggplot2)
+
+# simulating survival data
+library(simsurv)
+
+# Explore example data ----
+
+## psborrow2 contains an example matrix
+?example_matrix
+head(example_matrix)
+
+## load as data.frame for some functions
+example_dataframe <- as.data.frame(example_matrix)
+
+# Distribution of arms
+table(ext = example_matrix[,'ext'], trt = example_matrix[,'trt'])
+
+# Naive internal comparison ----
+
+## Cox model
+cox_fit <- coxph(Surv(time = time, event = 1 - cnsr) ~ trt,
+                 data = example_dataframe,
+                 subset = ext == 0)
+exp(confint(cox_fit))
+
+## Kaplan-meier curves
+km_fit <- survfit(Surv(time = time, event = 1 - cnsr) ~ trt,
+                  data = example_dataframe,
+                  subset = ext == 0)
+
+ggsurvplot(km_fit)
+
+## Exponential survival distribution
+exp_fit <- flexsurvreg(Surv(time = time, event = 1 - cnsr) ~ trt,
+                       dist = "exponential",
+                       data = example_dataframe,
+                       subset = ext == 0)
+exp(confint(exp_fit))
+
+# Hybrid control analysis----
+## The end goal
+?create_analysis_obj
+
+## Outcome class----
+?exp_surv_dist
+
+### A side note on priors
+?normal_prior
+plot(normal_prior(0, 1), xlim = c(-100, 100), ylim = c(0, 1))
+plot(normal_prior(0, 10), xlim = c(-100, 100), ylim = c(0, 1))
+plot(normal_prior(0, 10000), xlim = c(-100, 100), ylim = c(0, 1))
+
+### Create Outcome object
+exp_outcome <- exp_surv_dist(time_var = "time",
+                             cens_var = "cnsr",
+                             baseline_prior = normal_prior(0, 10000))
+
+## Borrowing class ----
+?borrowing_details
+
+bdb_borrowing <- borrowing_details(method = "BDB",
+                                   ext_flag_col = "ext",
+                                   tau_prior = gamma_prior(0.001, 0.001))
+
+## Treatment class ----
+?treatment_details
+
+trt_details <- treatment_details(trt_flag_col = "trt",
+                                 trt_prior = normal_prior(0, 10000))
+
+## Analysis class object ----
+analysis_object <- create_analysis_obj(
+  data_matrix = example_matrix,
+  outcome = exp_outcome,
+  borrowing = bdb_borrowing,
+  treatment = trt_details
+)
+
+## Sample from the MCMC sampler----
+?mcmc_sample
+
+results <- mcmc_sample(
+  x = analysis_object,
+  iter_warmup = 1000,
+  iter_sampling = 10000,
+  chains = 2
+)
+
+class(results)
+
+results$summary()
+
+## Dictionary to interpret results ----
+variable_dictionary(analysis_object)
+
+## Evaluate draws object----
