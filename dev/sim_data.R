45f9fa223d67c41101118788f0006db497c84ed2
Matt Secrest <secrestm@gene.com>
2022-10-11 07:29:07 -0700

    wip updating simdata

--- dev/sim_data.R
+++ dev/sim_data.R
@@ -8,8 +8,8 @@ make_prob <- function(odds) {
   odds / (1 + odds)
 }
 
-sim_single_matrix <- function(n = 500,
-                              prob = c(0.2, 0.4, 0.4),
+sim_single_matrix <- function(n = 400,
+                              prob = c(0.1, 0.2, 0.7),
                               hr = 0.7,
                               or = 1.20,
                               inherent_drift_hr = 1.0,
@@ -102,11 +102,11 @@ sim_single_matrix <- function(n = 500,
       ext = log(inherent_drift_hr),
       cov1 = log(cov1_hr),
       cov2 = log(cov2_hr),
-      cov3 = log(cov3_hr),
-      cov4 = log(cov4_hr)
+      cov3 = log(cov3_hr)#,
+      #cov4 = log(cov4_hr)
     ),
     x = cov,
-    maxt = 5
+    maxt = 20
   )
 
   dat$censor <- 1 - dat$status
@@ -118,8 +118,8 @@ sim_single_matrix <- function(n = 500,
   lp <- dat$trt * log(or) + dat$ext * log(inherent_drift_or) +
     dat$cov1 * log(cov1_or) +
     dat$cov2 * log(cov2_or) +
-    dat$cov3 * log(cov3_or) +
-    dat$cov4 * log(cov4_or)
+    dat$cov3 * log(cov3_or)# +
+    #dat$cov4 * log(cov4_or)
 
   exlp <- 1 / (1 + exp(-lp))
 
@@ -128,10 +128,10 @@ sim_single_matrix <- function(n = 500,
   dat <- dat[,c("id", "ext", "trt", "cov4", "cov3", "cov2", "cov1",
                 "eventtime", "status", "censor", "bin_endpoint")]
 
-  # Add propensity scores
-  ps_model <- glm(ext ~ cov1 + cov2 + cov3 + cov4, data = dat, family = binomial)
-  ps <- predict(ps_model, type = "response")
-  dat$ps <- ps
+  # # Add propensity scores
+  # ps_model <- glm(ext ~ cov1 + cov2 + cov3 + cov4, data = dat, family = binomial)
+  # ps <- predict(ps_model, type = "response")
+  # dat$ps <- ps
 
   as.matrix(dat)
 }
@@ -146,7 +146,7 @@ anls_noborrow <- create_analysis_obj(
   borrowing = borrowing_details("No borrowing", "ext"),
   treatment = treatment_details(trt_flag_col = "trt", trt_prior = normal_prior(0, 1000))
 )
-res_noborrow <- mcmc_sample(anls_noborrow, iter_sampling = 10000, chains = 4)
+res_noborrow <- mcmc_sample(anls_noborrow, iter_sampling = 10000, chains = 1)
 
 anls_borrow <- create_analysis_obj(
   examp_mat,
@@ -154,21 +154,33 @@ anls_borrow <- create_analysis_obj(
   borrowing = borrowing_details("BDB", "ext", gamma_prior(0.001, 0.001)),
   treatment = treatment_details(trt_flag_col = "trt", trt_prior = normal_prior(0, 1000))
 )
-res_borrow <- mcmc_sample(anls_borrow, iter_sampling = 10000, chains = 4)
+res_borrow <- mcmc_sample(anls_borrow, iter_sampling = 10000, chains = 1)
 
 anls_borrow_cov <- create_analysis_obj(
   examp_mat,
-  covariates = add_covariates(c("cov1", "cov2", "cov3", "cov4"), normal_prior(0, 1000)),
+  covariates = add_covariates(c("cov1", "cov2", "cov3"), normal_prior(0, 1000)),
   outcome = exp_surv_dist("eventtime", "censor", normal_prior(0, 1000)),
   borrowing = borrowing_details("BDB", "ext", gamma_prior(0.001, 0.001)),
   treatment = treatment_details(trt_flag_col = "trt", trt_prior = normal_prior(0, 1000))
 )
-res_borrow_cov <- mcmc_sample(anls_borrow_cov, iter_sampling = 10000, chains = 4)
+res_borrow_cov <- mcmc_sample(anls_borrow_cov, iter_sampling = 10000, chains = 1)
 
-res_noborrow_tbl <- summarize_draws(res_noborrow$draws(), ~ quantile(.x, probs = c(0.025, 0.975)))
-res_borrow_tbl <- summarize_draws(res_borrow$draws(), ~ quantile(.x, probs = c(0.025, 0.975)))
-res_borrow_tbl_cov <- summarize_draws(res_borrow_cov$draws(), ~ quantile(.x, probs = c(0.025, 0.975)))
+res_noborrow_tbl <- summarize_draws(res_noborrow$draws(), ~ quantile(.x, probs = c(0.025, 0.5, 0.975)))
+res_borrow_tbl <- summarize_draws(res_borrow$draws(), ~ quantile(.x, probs = c(0.025, 0.5, 0.975)))
+res_borrow_tbl_cov <- summarize_draws(res_borrow_cov$draws(), ~ quantile(.x, probs = c(0.025, 0.5, 0.975)))
 
 res_noborrow_tbl
 res_borrow_tbl
 res_borrow_tbl_cov
+
+ggsurvplot(survfit(Surv(eventtime, status) ~ trt + ext, data = examp_df))
+coxph(Surv(eventtime, status) ~ trt, data = examp_df, subset = ext == 0)
+coxph(Surv(eventtime, status) ~ trt + cov1 + cov2 + cov3, data = examp_df, subset = ext == 0)
+
+fstrt <- flexsurvreg(Surv(eventtime, status) ~ trt, dist = "exponential", data = examp_df, subset = ext == 0)
+exp(coef(fstrt))
+exp(confint(fstrt))
+
+fstrtcov <- flexsurvreg(Surv(eventtime, status) ~ trt +cov1 + cov2 + cov3, dist = "exponential", data = examp_df, subset = ext == 0)
+exp(coef(fstrtcov))
+exp(confint(fstrtcov))
