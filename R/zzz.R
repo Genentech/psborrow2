ee297c608f76f47ddd3f299a61cec431c998d15b
gravesti <isaac.gravestock@roche.com>
2022-10-13 13:00:29 +0200

    only use packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -12,12 +12,12 @@
     if (length(possible_paths)) {
       for (try_path in possible_paths) {
         new_path <- tryCatch(
-          cmdstanr::set_cmdstan_path(try_path),
+          suppressMessages(cmdstanr::set_cmdstan_path(try_path)),
           warning = function(w) NULL,
           error = function(e) NULL
         )
         if (!is.null(new_path)) {
-          packageStartupMessage("CmdStan path set to ", new_path)
+          packageStartupMessage("CmdStan path set to: ", new_path)
           return(invisible(NULL))
         }
       }
