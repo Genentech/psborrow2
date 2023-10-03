e33533cedc35345a190a2de795d1ad3428a49729
Isaac Gravestock <83659704+gravesti@users.noreply.github.com>
2023-10-03 07:58:02 +0200

    baseline data simulation (#218)
    * first design
    
    * more ideas
    
    * reduce scope
    
    * implementation sketch
    
    * fix checks
    
    * add show
    
    * small fixes
    
    * use release
    
    * [skip actions] Restyle files
    
    * improve docs
    
    * fix doc links
    
    * add vignette
    
    * rename
    
    * rename to lowercase
    
    * fix title
    
    * vignette title
    
    * as.integer for cutoff
    
    * wip
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Matt Secrest <secrestm@gene.com>

--- tests/testthat/test-cmdstan.R
+++ tests/testthat/test-cmdstan.R
@@ -1,8 +1,12 @@
 test_that("check_cmdstanr runs as expected", {
   expect_output(
-    result <- check_cmdstanr(),
+    check_cmdstanr(check_sampling = FALSE),
     "int<lower=0> N;",
     fixed = TRUE
   )
+
+  skip_on_ci()
+  skip_on_cran()
+  result <- check_cmdstanr(check_sampling = TRUE)
   expect_class(result, "CmdStanMCMC")
 })
