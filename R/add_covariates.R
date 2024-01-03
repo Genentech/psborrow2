374ab36524eda23f4bac52740c6112cf1df52248
Matt Secrest <secrestm@gene.com>
2024-01-03 11:48:19 -0800

    make covariate parent class

--- R/add_covariates.R
+++ R/add_covariates.R
@@ -1,3 +1,91 @@
+# class union----
+setClassUnion("listOrPrior", c("list", "Prior"))
+
+#' `Covariates` Class
+#'
+#' A class for defining covariate details. Objects of class
+#' `Covariate` should not be created directly but by the constructor
+#' [add_covariates()].
+#'
+#' @slot covariates character. Names of columns in the data matrix containing
+#' covariates to be adjusted for in the outcome model. Note: the
+#' external and treatment flags should not go here.
+#' @slot priors. Either a single object of class `Prior` specifying the prior
+#' distribution to apply to all covariates or a named list of
+#' distributions of class `Prior`, one for each covariate
+#' @slot name_betas. Names for the beta parameters in the STAN model.
+#' @slot n_cov. Number of covariates to adjust for.
+#' @family covariate classes
+#' @include prior_class.R
+.covariate_class <- setClass(
+  "Covariates",
+  slots = c(
+    covariates = "character",
+    priors = "listOrPrior",
+    name_betas = "character"
+  ),
+  validity = function(object) {
+    if (is(object@priors, "list")) {
+      if (NROW(object@priors) != NROW(object@covariates)) {
+        return(paste0(
+          "Either specify 1 prior distribution for all ",
+          "covariates, or specify a named list with 1 prior per ",
+          "covariate"
+        ))
+      }
+
+      if (!all(vapply(object@priors, is, logical(1L), class2 = "Prior"))) {
+        return(paste0(
+          "If a list is provided to specify priors, all priors ",
+          "must be of class `Prior`"
+        ))
+      }
+
+      if (!all(names(object@priors) %in% object@covariates)) {
+        return(paste0(
+          "If a list is provided to specify priors, one prior per ",
+          "covariate must be provided."
+        ))
+      }
+    }
+
+    return(TRUE)
+  },
+  contains = "CovariateClass"
+)
+
+# show ----
+setMethod(
+  f = "show",
+  signature = "Covariates",
+  definition = function(object) {
+    cat("Covariate object with priors for variables:\n")
+    cat(toString(object@covariates), "\n\n")
+    if (is(object@priors, "Prior")) {
+      cat("Prior for all covariate coefficients:\n")
+      show(object@priors)
+    } else if (is(object@priors, "list")) {
+      for (i in object@covariates) {
+        cat("Prior for ", i, ":\n", sep = "")
+        show(object@priors[[i]])
+        cat("\n")
+      }
+    }
+  }
+)
+
+# get_vars ----
+#' @rdname get_vars
+#' @include generics.R
+setMethod(
+  f = "get_vars",
+  signature = "Covariates",
+  definition = function(object) {
+    c(object@covariates)
+  }
+)
+
+
 #' Add Covariates for Model Adjustment
 #'
 #' Specify column names for adjustment variables in model matrix and prior
@@ -35,6 +123,7 @@ add_covariates <- function(covariates,
   .covariate_class(
     covariates = covariates,
     priors = priors,
-    name_betas = stats::setNames(h_glue("beta[{{seq_along(covariates)}}]"), covariates)
+    name_betas = stats::setNames(h_glue("beta[{{seq_along(covariates)}}]"), covariates),
+    n_cov = NROW(covariates)
   )
 }
--- R/covariate_class.R
+++ R/covariate_class.R
@@ -1,83 +1,16 @@
-# class union----
-setClassUnion("listOrPrior", c("list", "Prior"))
-
-#' `Covariate` Class
+#' `CovariateClass` Class
 #'
-#' A class for defining covariate details. Objects of class
-#' `Covariate` should not be created directly but by the constructor
+#' A class for defining covariate adjustment details. Objects of class
+#' `CovariateClass` should not be created directly but by the constructor
 #' [add_covariates()].
 #'
-#' @slot covariates character. Names of columns in the data matrix containing
-#' covariates to be adjusted for in the outcome model. Note: the
-#' external and treatment flags should not go here.
-#' @slot priors. Either a single object of class `Prior` specifying the prior
-#' distribution to apply to all covariates or a named list of
-#' distributions of class `Prior`, one for each covariate
-#' @slot name_betas. Names for the beta parameters in the STAN model.
-#' @include prior_class.R
-.covariate_class <- setClass(
-  "Covariates",
+#' @slot n_covs integer. Number of covariates to adjust for
+#' @family covariate classes
+#' @seealso Prior constructor functions: [add_covariates()], [no_covariates()]
+setClass(
+  "CovariateClass",
   slots = c(
-    covariates = "character",
-    priors = "listOrPrior",
-    name_betas = "character"
+    n_covs = "integer"
   ),
-  validity = function(object) {
-    if (is(object@priors, "list")) {
-      if (NROW(object@priors) != NROW(object@covariates)) {
-        return(paste0(
-          "Either specify 1 prior distribution for all ",
-          "covariates, or specify a named list with 1 prior per ",
-          "covariate"
-        ))
-      }
-
-      if (!all(vapply(object@priors, is, logical(1L), class2 = "Prior"))) {
-        return(paste0(
-          "If a list is provided to specify priors, all priors ",
-          "must be of class `Prior`"
-        ))
-      }
-
-      if (!all(names(object@priors) %in% object@covariates)) {
-        return(paste0(
-          "If a list is provided to specify priors, one prior per ",
-          "covariate must be provided."
-        ))
-      }
-    }
-
-    return(TRUE)
-  }
-)
-
-# show ----
-setMethod(
-  f = "show",
-  signature = "Covariates",
-  definition = function(object) {
-    cat("Covariate object with priors for variables:\n")
-    cat(toString(object@covariates), "\n\n")
-    if (is(object@priors, "Prior")) {
-      cat("Prior for all covariate coefficients:\n")
-      show(object@priors)
-    } else if (is(object@priors, "list")) {
-      for (i in object@covariates) {
-        cat("Prior for ", i, ":\n", sep = "")
-        show(object@priors[[i]])
-        cat("\n")
-      }
-    }
-  }
-)
-
-# get_vars ----
-#' @rdname get_vars
-#' @include generics.R
-setMethod(
-  f = "get_vars",
-  signature = "Covariates",
-  definition = function(object) {
-    c(object@covariates)
-  }
+  contains = "VIRTUAL"
 )
