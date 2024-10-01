7ed3c7b89ca37a8cbb2873edca12fbb10989940e
Matt Secrest <matthewhsecrest@gmail.com>
2024-09-30 18:10:04 -0700

    make model string model

--- R/make_model_string_model.R
+++ R/make_model_string_model.R
@@ -68,6 +68,67 @@ setMethod("make_model_string_model", signature("BorrowingFull", "ANY", "Analysis
 #' @rdname make_model_string_model
 setMethod("make_model_string_model", signature("BorrowingNone", "ANY", "Analysis"), make_model_string_full_none)
 
+make_model_string_full_none_pem <- function(borrowing, outcome, analysis_obj) {
+  ### Treatment prior
+  beta_trt_prior <- get_prior_string(analysis_obj@treatment@trt_prior)
+
+  ### Linear predictor
+  has_covariates <- !is.null(analysis_obj@covariates)
+
+  if (has_covariates) {
+    linear_predictor <- h_glue("
+      lp = Z * alpha + X * beta + trt * beta_trt ;
+      elp = exp(lp) ;")
+  } else if (!has_covariates) {
+    linear_predictor <- h_glue("
+      lp = Z * alpha + trt * beta_trt ;
+      elp = exp(lp);")
+  }
+
+  ### Add priors for relevant parameters
+  if (NROW(analysis_obj@outcome@param_priors) > 0) {
+    names <- names(analysis_obj@outcome@param_priors)
+    values <- get_prior_string(analysis_obj@outcome@param_priors)
+    outcome_prior <- h_glue("{{names}} ~ {{values}} ;", collapse = TRUE)
+  } else {
+    outcome_prior <- ""
+  }
+
+  ### Add priors on betas
+  if (has_covariates) {
+    i <- seq_along(analysis_obj@covariates@covariates)
+    value <- get_prior_string(analysis_obj@covariates@priors)
+    index <- if (test_named(value)) get_vars(analysis_obj@covariates) else rep(1, length(i))
+    covariate_prior <- h_glue("beta[{{i}}] ~ {{value[index]}} ;", collapse = TRUE)
+  } else {
+    covariate_prior <- ""
+  }
+
+  alpha_prior <- get_prior_string(analysis_obj@outcome@baseline_prior)
+  borrowing_string <- h_glue("alpha ~ {{alpha_prior}} ;")
+
+  ### Add in likelihood function
+  likelihood_string <- h_glue("{{analysis_obj@outcome@likelihood_stan_code}}")
+
+  h_glue("
+  model {
+    vector[N] lp;
+    vector[N] elp;
+    beta_trt ~ {{beta_trt_prior}};
+    {{linear_predictor}}
+    {{outcome_prior}}
+    {{covariate_prior}}
+    {{borrowing_string}}
+    {{likelihood_string}}
+  }")
+}
+
+#' @rdname make_model_string_model
+setMethod("make_model_string_model", signature("BorrowingFull", "OutcomeSurvPEM", "Analysis"), make_model_string_full_none_pem)
+
+#' @rdname make_model_string_model
+setMethod("make_model_string_model", signature("BorrowingNone", "OutcomeSurvPEM", "Analysis"), make_model_string_full_none_pem)
+
 #' @rdname make_model_string_model
 setMethod(
   "make_model_string_model",
@@ -134,7 +195,6 @@ setMethod(
   }")
   }
 )
-
 
 #' @rdname make_model_string_model
 #' @include outcome_surv_pem.R
